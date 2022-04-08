f95cae93504e59591ea389f7d470a0121db2a14a
PatriciaSkowronek <skowronek@biochem.mpg.de>
2022-04-08 14:21:50 +0200

    FIX: change pyinstaller to 4.10

--- release/one_click_windows_gui/create_installer_windows.sh
+++ release/one_click_windows_gui/create_installer_windows.sh
@@ -20,7 +20,7 @@ cd release/one_click_windows_gui
 pip install "../../dist/pydiaid-0.0.12-py3-none-any.whl[stable]"
 
 # Creating the stand-alone pyinstaller folder
-pip install pyinstaller==4.2
+pip install pyinstaller==4.10
 pyinstaller ../pyinstaller/pydiaid.spec -y
 conda deactivate
 
