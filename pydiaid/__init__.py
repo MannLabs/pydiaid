f31c8a8ce92a6c24faf33e086f31b84b1d41ea8e
Patricia Skowronek <skowronek@biochem.mpg.de>
2024-01-10 13:01:28 +0100

    Bump version: 0.0.21 → 0.0.22

--- pydiaid/__init__.py
+++ pydiaid/__init__.py
@@ -2,7 +2,7 @@
 
 
 __project__ = "pydiaid"
-__version__ = "0.0.21"
+__version__ = "0.0.22"
 __license__ = "Apache"
 __description__ = "An open-source Python package of the AlphaPept ecosystem"
 __author__ = "Mann Labs"
