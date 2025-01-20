f7acf0514fa003c674f6662039ef9087cf92f1ba
PatriciaSkowronek <skowronek@biochem.mpg.de>
2025-01-20 10:29:27 +0100

    Fix: reverting changes, packages installed in windows installer

--- release/one_click_windows_gui/create_installer_windows.sh
+++ release/one_click_windows_gui/create_installer_windows.sh
@@ -17,10 +17,14 @@ python setup.py sdist bdist_wheel
 # Setting up the local package
 cd release/one_click_windows_gui
 # Make sure you include the required extra packages and always use the stable or very-stable options!
-pip install "../../dist/pydiaid-0.0.33-py3-none-any.whl[stable]"
+pip install "../../dist/pydiaid-0.0.32-py3-none-any.whl[stable]"
 
 # Creating the stand-alone pyinstaller folder
 pip install pyinstaller==4.10
+
+conda install -y -c conda-forge --strict-channel-priority scikit-optimize=0.9.0 numpy=1.23.5 openblas=0.3.23 scipy=1.10.1 pandas=1.3.4 matplotlib=3.6.0
+    # geos ^
+
 pyinstaller ../pyinstaller/pydiaid.spec -y --clean
 conda deactivate
 
