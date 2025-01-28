ac3ad009851a9f1ddc0eacc4c4a7766bdf12e5a0
Patricia Skowronek <skowronek@biochem.mpg.de>
2025-01-28 11:37:53 +0100

    Fix: removed unnecessary comment

--- release/one_click_windows_gui/create_installer_windows.sh
+++ release/one_click_windows_gui/create_installer_windows.sh
@@ -23,7 +23,6 @@ pip install "../../dist/pydiaid-0.0.32-py3-none-any.whl[stable]"
 pip install pyinstaller==4.10
 
 conda install -y -c conda-forge --strict-channel-priority scikit-optimize=0.9.0 numpy=1.23.5 openblas=0.3.23 scipy=1.10.1 pandas=1.3.4 matplotlib=3.6.0
-    # geos ^
 
 pyinstaller ../pyinstaller/pydiaid.spec -y --clean
 conda deactivate
