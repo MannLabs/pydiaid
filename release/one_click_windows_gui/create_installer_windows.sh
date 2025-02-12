b1ab2919a138a0befea6c072bade22b839f9b4b3
Patricia Skowronek <skowronek@biochem.mpg.de>
2025-02-12 14:08:50 +0100

    FIX: Only using conda-forge

--- release/one_click_windows_gui/create_installer_windows.sh
+++ release/one_click_windows_gui/create_installer_windows.sh
@@ -7,6 +7,10 @@ cd ../..
 rm -rf dist
 rm -rf build
 
+conda init
+conda config --remove channels defaults
+conda config --add channels conda-forge
+
 # Creating a conda environment
 conda create -n pydiaid_installer python=3.8 -y
 conda activate pydiaid_installer
@@ -22,7 +26,7 @@ pip install "../../dist/pydiaid-0.0.36-py3-none-any.whl[stable]"
 # Creating the stand-alone pyinstaller folder
 pip install pyinstaller==4.10
 
-conda install -y -c conda-forge --strict-channel-priority scikit-optimize=0.9.0 numpy=1.23.5 openblas=0.3.23 scipy=1.10.1 pandas=1.3.4 matplotlib=3.6.0
+conda install -y scikit-optimize=0.9.0 numpy=1.23.5 openblas=0.3.23 scipy=1.10.1 pandas=1.3.4 matplotlib=3.6.0
 
 pyinstaller ../pyinstaller/pydiaid.spec -y --clean
 conda deactivate
